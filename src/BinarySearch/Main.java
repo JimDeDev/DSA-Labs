49b0fcb21175d25353536eff9dad24564959a4d8
JimDeDev <jaimeking00@gmail.com>
2019-04-04 00:42:41 +1300

    Added Binary Search

--- src/BinarySearch/Main.java
+++ src/BinarySearch/Main.java
@@ -1,27 +1,56 @@
 
 package BinarySearch;
 
-
-import CircularArrayQueue.ArrayQueue;
-
-import java.awt.*;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  *
  * @author gjr5542
  */
 public class Main {
-    
+
+     private static LinkedList<Integer> array;
+
      public static void main(String[] args) {
 
+          array = new LinkedList<>();
 
-          ArrayList<Integer> array = new ArrayList<>();
+          for (int i = 0; i < 50; i += 5) {
+               array.add(i);
+          }
 
-          //for (int i = 0;
+          System.out.println(array);
 
+          int target = 30;
 
+          int targetIndex = search(target, 0, array.size());
 
+          System.out.print("Found " + target + " at index " + targetIndex);
 
+     }
+
+     private static int search(Integer target, int start, int end) {
+
+          if (start > end) {
+               return -start-1;
+          }
+
+          ListIterator li = array.listIterator();
+          int middle = (start + end ) / 2;
+          Integer middleElement = -1;
+          for(int i = 0; i <= middle; i++) {
+               middleElement = (Integer) li.next();
+          }
+
+          if (target.compareTo(middleElement) == 0) {
+               return middle;
+          }
+          else if(target.compareTo(middleElement) < 0 ) {
+               return search(target, start, middle);
+          }
+          else {
+               return search(target, middle + 1, end);
+          }
      }
 }
